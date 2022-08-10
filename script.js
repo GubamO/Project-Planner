347e79bdc46df99e195198b82af3b85780b41e87
GuillaumeB <guibau2303@gmail.com>
2022-08-10 12:04:13 +0200

    début tableau

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const theBody = document.body;
 theBody.style.background = "blue";
+
+const table = document.createElement('table');
+const thead = document.createElement('thead');
+const tr = document.createElement('tr');
+const th = document.createElement('th');
+console.log(table, thead, tr, th);
+
+
