155eb7763e8b818099c0667295b5f909c75bfa35
Melonde-Zeus <vervoorteddy@outlook.com>
2022-08-10 11:28:14 +0200

    add arrayOblect + input for creat object

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-const theBody = document.body;
-theBody.style.background = "blue";
+let ToDoList = [];
+const theMain = document.querySelector("main");
+
+const aInput = document.createElement("input");
+theMain.appendChild(aInput);
+
+const aBut = document.createElement("button");
+aBut.innerText= "name";
+theMain.appendChild(aBut);
+
+aBut.addEventListener("click",() => {
+    let object =[];
+    switch(aBut.innerText) {  
+        case "name": object.push(`${aBut.innerText} : ${aInput.value}`);
+            aBut.innerText = "descript";
+         break;
+        case "descript": object.push(`${aBut.innerText} : ${aInput.value}`);
+            aBut.innerText = "Due Date";
+         break;
+        case "Due Date": object.push(`${aBut.innerText} : ${aInput.value}`);
+            aInput.style.visibility = "hidden"; 
+            aBut.innerText = "val to do";
+        
+         break;
+        case "val to do": ToDoList.push(object);
+             aBut.innerText = "name"; 
+             aInput.style.visibility= "visible";
+             object = [];
+         break;
+        } 
+    aInput.value = "";
+        
+});
+ 
+//setInterval(() => {console.log(ToDoList)},1000);
