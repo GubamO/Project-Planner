c22a43bdb5508eed519bf6373240bf9da40925f7
Melonde-Zeus <vervoorteddy@outlook.com>
2022-08-11 16:22:19 +0200

    add but urg( move element in first pos) + select operationel

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ const aBut = document.createElement("button");
 aBut.innerText= "name";
 theMain.appendChild(aBut);
 
-
 aBut.addEventListener("click",() => setInput());
 aInput.addEventListener("keyup", event => {
     if(event.key == "Enter" ){
         setInput();
     }
 });
-
+table();
 
 let articleElement = document.createElement("article");
 function setInput(){   
@@ -53,6 +52,7 @@ function setInput(){
             
             articleElement.appendChild(descriptArt);
             const selectArt = document.createElement("select");
+            selectArt.classList.add("selectPos");
             const nameOpt = ["to do","Doing","Done","trach"];
             for(let i = 0; i < nameOpt.length; i++){
                 const optionSelect = document.createElement("option");
@@ -60,35 +60,79 @@ function setInput(){
                 selectArt.appendChild(optionSelect);
             }
             articleElement.appendChild(selectArt);
+            const butUrg = document.createElement("button");
+            butUrg.innerText = "Urgent";
+            butUrg.classList.add("urgent");
+            articleElement.appendChild(butUrg);
             const colToDo = document.querySelector(".ToDo");
             colToDo.appendChild(articleElement);
             articleElement = document.createElement("article");
-            addevtBut();
+            ShowDescript();
+            changeSelect();
+            todoUrgent();
             aBut.innerText = "name"; 
             aInput.style.visibility= "visible";
          break;
     } 
     aInput.value = "";
 }
-function addevtBut(){
+function ShowDescript(){
     const but = document.getElementsByClassName("showD");
     for(let item of but){
-    item.addEventListener("click",() =>{
+        item.addEventListener("click",() =>{
         const target = item.parentNode.querySelector(".description");
-    if(target.style.display == "block"){
-        target.style.display = "none";
-    }   
-    else{
-        target.style.display = "block";
+        if(target.style.display == "block"){
+            target.style.display = "none";
+        }   
+        else{
+            target.style.display = "block";
+        }}); 
     }
-
- }); 
-}
 }
 
+function changeSelect(){
+    const theSelcet = document.getElementsByClassName("selectPos");
 
-table();
+    for(let item of theSelcet){
+        item.addEventListener("change",actionSelect);
+    }
+}
+function actionSelect(event){
+    const colTodo = document.querySelector(".ToDo");
+    const colDoing = document.querySelector(".Doing");
+    const colDone = document.querySelector(".Done");
+    const targetParent = event.target.parentNode
+    const parentArt = targetParent.parentNode;
+    switch(event.target.value){
+        case "to do":parentArt.removeChild(targetParent); 
+                    colTodo.appendChild(targetParent);
+                break;
+        case "Doing":parentArt.removeChild(targetParent);
+                    colDoing.appendChild(targetParent);
+                break;
+        case "Done" :parentArt.removeChild(targetParent); 
+                    colDone.appendChild(targetParent);
+                break;
+        case "trach":confirm("are you sure ?")? parentArt.removeChild(targetParent):0;
+             break;
+    }
+}
 
+function todoUrgent(){
+    const butUrgent = document.querySelectorAll(".urgent")
+    for( let item of butUrgent){
+        item.addEventListener("click",first );
+    }
+}
+ function first(event) {
+            const elementFirst = event.target.parentNode;
+            
+            const parentElement = elementFirst.parentNode;
+            console.log(parentElement);
+            parentElement.insertBefore(elementFirst,parentElement.childNodes[1]);
+            
+
+        }
 function table(){
     const titleDiv = ["ToDo","Doing","Done"];
     for(let i = 0; i < titleDiv.length; i++){
